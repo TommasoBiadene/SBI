336a87b7c8c847d6a17754052a41176ebedb77bd
TommasoBiadene <tbiadene@gmail.com>
2023-11-07 15:42:15 +0100

    mannaggia non funziona il tutorial indiano

--- app/src/main/java/com/example/sbi/MainActivity.java
+++ app/src/main/java/com/example/sbi/MainActivity.java
@@ -1,14 +1,39 @@
 package com.example.sbi;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
+import com.example.sbi.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+
+            switch (item.getItemId()){
+
+                case R.id.home:
+                    break;
+            }
+            return  true;
+        });
+    }
+
+    private void replaceFragment(Fragment f){
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.replace(R.id.frame_layout,f);
+        ft.commit();
     }
 }
